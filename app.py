718f97dc3538a9a01382c4934e14d984c3ba09af
Natalia Lubaale <natalialubaale@Natalias-MacBook-Pro.local>
2026-08-08 12:23:14 +0100

    Initial AI Study Assistant project

---
+++ app.py
@@ -0,0 +1,11 @@
+import notes
+
+print ("Welcome to my AI Study Assistant!")
+
+my_note = notes.create_note(
+    "Python Variables",
+    "Variables store information.",
+    "Programming"
+)
+
+print(my_note)
---
+++ notes.py
@@ -0,0 +1,13 @@
+def create_note(title, content, subject):
+    """
+    Creates a note using the information provided.
+    """
+    note = {
+        "title": title,
+        "content": content,
+        "subject": subject
+    }
+    
+    return note
+    
+    
