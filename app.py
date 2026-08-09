d9c50b01567f268ef51a8f4fac0c370f78a2ada0
Natalia Lubaale <natalialubaale@Natalias-MacBook-Pro.local>
2026-08-09 14:32:21 +0100

    Added functions to add, display and search for notes

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ import notes
 
 print ("Welcome to my AI Study Assistant!")
 
+notes_list = []
+
 my_note = notes.create_note(
     "Python Variables",
     "Variables store information.",
     "Programming"
 )
 
-print(my_note)
+my_note2 = notes.create_note(
+    "Generative AI", 
+    "Relies on deep learning algorithms.",
+    "AI"
+)
+
+my_note3 = notes.create_note(
+    "Functions",
+    "A function is a relation from one set to another.",
+    "Mathematics"
+)
+
+notes.add_note(notes_list, my_note)
+notes.add_note(notes_list, my_note2)
+notes.add_note(notes_list, my_note3)
+
+notes.display_notes(notes_list)
+
+print("\nSearching for Python...\n")
+
+notes.search_notes(notes_list, "Python")
+
--- notes.py
+++ notes.py
@@ -10,4 +10,36 @@ def create_note(title, content, subject):
     
     return note
     
+def add_note(notes_list, note):
+    """
+    Adds a note to the list of notes.
+    """
     
+    notes_list.append(note)
+    
+def display_notes(notes_list):
+    """
+    Displays all notes in a readable format.
+    """
+    
+    for note in notes_list:
+        print("---------------------------")
+        print("Title:", note["title"])
+        print("Content:", note["content"])
+        print("Subject:", note["subject"])
+        print("---------------------------")
+        
+def search_notes(notes_list, keyword):
+    """
+    Searches for a keyword in the note titles.
+    """
+    
+    for note in notes_list:
+        if (keyword.lower() in note ["title"].lower()
+            or keyword.lower() in note["content"].lower()
+                or keyword.lower in note["subject"].lower()):
+            print("---------------------------")
+            print("Title:", note["title"])
+            print("Content:", note["content"])
+            print("Subject:", note["subject"])
+            print("---------------------------")
